4684b96fcf2728334d98017e822beae78e860f2a
hsianlunghou <145786937+hsianlunghou@users.noreply.github.com>
2025-11-17 21:48:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -389,7 +389,7 @@ start_processing_thread()
 
 if __name__ == '__main__':
     try:
-        app.run(debug = True, threaded = True, host = '0.0.0.0', port = 5000)
+        app.run(debug = False, threaded = True, host = '0.0.0.0', port = 5000)
     finally:
         # Cleanup on shutdown
         stop_processing_thread()
